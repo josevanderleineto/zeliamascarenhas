261d1591e5c84fd81d3e519f84aacf7aca9afe76
Vanderlei Neto <jv.neto@outlook.com.br>
2023-06-21 21:37:16 -0300

    Saubino projeto

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,16 @@ const menuToggle = document.querySelector('.menu-toggle');
 const sidebar = document.querySelector('.sidebar');
 
 menuToggle.addEventListener('click', () => {
-  console.log("click")
+  console.log("click");
   sidebar.classList.toggle('active');
 });
 
+document.addEventListener('click', (event) => {
+  const target = event.target;
+  const isMenuOpen = sidebar.classList.contains('active');
 
+  // Verifica se o clique ocorreu dentro ou fora do menu
+  if (isMenuOpen && !target.closest('.sidebar') && !target.closest('.menu-toggle')) {
+    sidebar.classList.remove('active');
+  }
+});

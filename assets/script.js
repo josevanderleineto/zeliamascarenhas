8ea48a8f0c2a9e9d94773625731a3ede2132ea18
Vanderlei Neto <jv.neto@outlook.com.br>
2023-07-01 19:01:43 -0300

    Env form

--- assets/script.js
+++ assets/script.js
@@ -15,3 +15,45 @@ document.addEventListener('click', (event) => {
     sidebar.classList.remove('active');
   }
 });
+
+
+//Fomulario
+const form = document.querySelector("#form");
+const inputName = document.querySelector("#name")
+const inputEmail = document.querySelector("#email");
+const textareaMensagem = document.querySelector("#mensagem");
+
+form.addEventListener("submit", (event =>{
+    event.preventDefault();
+
+    if(inputName.value === "") {
+        alert("Escreva seu nome!");
+        return;
+    }
+
+    if(inputEmail.value === "" || !isEmailValid(inputEmail.value)) {
+        alert("Por favor prehencha seu email");
+        alert("Seu email deve ter o formato fulano@domio.com")
+        return;
+    }
+
+    if(textareaMensagem.value === "") {
+        alert("Escreva sua mendagem!")
+    }
+
+    form.submit();
+}));
+
+const isEmailValid = (email) => {
+    
+    const emailRegex = new RegExp(
+        //usuario12@host.com.br
+        /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,}/
+    )
+
+    if(emailRegex.test(email)) {
+        return true;
+    }
+
+    return false;
+}
